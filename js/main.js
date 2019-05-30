4c404280928ce0a3c1a87e42582f76cf70bede3b
Siddiqui <Omer.Siddiqui@CenturyLink.com>
2019-05-30 16:35:08 -0600

    added ajax loading gif

--- js/main.js
+++ js/main.js
@@ -2,11 +2,12 @@ var api_key = 'TtEgkQPN6mRRNi6zKm155Jkmc950XLU8';
 var content = $('#content');
 
 
-$('#submit').on('click', function() {
+$('#submit').on('click', function(e) {
+    e.preventDefault();
     var input = $('#input').val();
 
     $.ajax({
-        url: `https://api.giphy.com/v1/gifs/search?api_key=${api_key}&lang=en&limit=16&q=${input}`,
+        url: `https://api.giphy.com/v1/gifs/search?api_key=${api_key}&lang=en&limit=8&q=${input}&rating=g`,
         method: 'GET',
         dataType: 'json',
         success: function(data) {
@@ -14,14 +15,18 @@ $('#submit').on('click', function() {
             content.empty();
 
             $('#image').css("display", "none")
+            content.append('<img id="loading" src="/assets/images/loading.gif" alt="gif from Giphy"/>')
 
             for(var i = 0; i < data.data.length; i++) {
                 var gifUrl = data.data[i].images.fixed_height.url;
-                content.append('<img src="' + gifUrl + '" alt="gif from Giphy"/>' )
+                content.append('<img class="hideElement contentGifs" src="' + gifUrl + '" alt="gif from Giphy"/>' )
             }
 
-
-            // $('#image')[0].src = `${gifFromResponse}`
+            setTimeout(function() {
+                var contentGifs = $('.contentGifs');
+                $("#loading").hide();
+                contentGifs.css("display", "inline-block");
+            }, 3000)
         },
         error: function(err) {
             console.log(err);
